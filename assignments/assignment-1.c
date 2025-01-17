872a59e5e2477efeca73198f52f3c9e5cf0ebe36
Zaw Nay Lin <zawnay160399@gmail.com>
2025-01-17 09:43:08 +0700

    [add] assignments with while

---
+++ assignments/assignment-1.c
@@ -0,0 +1,15 @@
+// Write a program that finds the largest in a series of numbers entered by the
+// user. The program must prompt the user to enter numbers one by one. When
+// the user enters O or a negative number, the program must display the largest
+// non-negative number entered.
+// Notice that the numbers aren't necessarily integers.
+//
+// Example Program
+//
+// Enter a number: 60
+// Enter a number: 38.3
+// Enter a number: 4.89
+// Enter a number: 100.62
+// Enter a number: 75.2295
+// Enter a number: 0
+// The largest number entered was l00.62
---
+++ assignments/assignment-2.c
@@ -0,0 +1,11 @@
+// Write a program that prompts the user to enter a number n, then prints all
+// even squares between 1 and n.
+//
+// Example Program 
+// 
+// Enter the number: 100
+// 4
+// 16
+// 36
+// 64
+// 100
