129d95de3f64e90ddeaee8b1175f69e15ae3e5a7
Zaw Nay Lin <zawnay160399@gmail.com>
2025-01-22 18:10:15 +0700

    [add] assignment-1

---
+++ assignments/assignment-1.c
@@ -0,0 +1,20 @@
+// Write a number guessing game, which will generate an integer number between 1
+// and 100, and let the user to guess the number within 5 turns. After each
+// guess, the program will tell if the number is higher or lower. If the user is
+// out of guesses, the program will show the number generated.
+// After the game, the program will prompt the user to play again or not. If 'y'
+// is typed, the game will restart, else the program will terminate.
+//
+// For example, suppose the program generate random number of 4
+//
+// Program Execution Example:
+// Guess the number (5 guesses remaining): 10
+// Wrong, it is lower.
+// Guess the number (4 guesses remaining): 5
+// Wrong, it is lower.
+// Guess the number (3 guesses remaining): 2
+// Wrong, it is higher.
+// Guess the number (2 guesses remaining): 4
+// Correct! The magic number is 4.
+// Would you like to play again? n
+// Have a wonderful day!
