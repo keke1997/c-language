9e8bb8e10897df2a837fe57609c1da70d3567e17
htug <candy.hipperboy112@gmail.com>
2025-01-28 18:38:17 +0800

    gay

--- assignments/assignment-1.c
+++ assignments/assignment-1.c
@@ -18,3 +18,78 @@
 // Correct! The magic number is 4.
 // Would you like to play again? n
 // Have a wonderful day!
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+// Function to ask the user a yes or no question
+int yes_or_no(const char *prompt) {
+    char response;
+    while (1) {
+        printf("%s (y/n): ", prompt);
+        scanf(" %c", &response); // Space before %c to consume whitespace
+        response = tolower(response); // Normalize to lowercase
+        if (response == 'y') {
+            return 1; // Yes
+        } else if (response == 'n') {
+            return 0; // No
+        } else {
+            printf("Invalid input. Please enter 'y' or 'n'.\n");
+        }
+    }
+}
+
+// Function for the number guessing game
+void guess(int N) {
+    int number, guess, numberofguess;
+
+    // Seed random number generator
+    srand(time(NULL));
+
+    do {
+        // Initialize variables
+        number = rand() % N + 1; // Generate a number between 1 and N
+        numberofguess = 5;
+
+        printf("\nI have generated a number between 1 and %d. Can you guess it?\n", N);
+
+        // Game loop
+        do {
+            printf("Guess the number (%d guesses remaining): ", numberofguess);
+            scanf("%d", &guess);
+
+            if (guess > number) {
+                printf("Wrong, it is lower.\n");
+            } else if (guess < number) {
+                printf("Wrong, it is higher.\n");
+            } else {
+                printf("Correct! The magic number is %d.\n", number);
+                break;
+            }
+
+            numberofguess--;
+
+            if (numberofguess == 0) {
+                printf("You are out of guesses! The correct number was %d.\n", number);
+                break;
+            }
+
+        } while (numberofguess > 0);
+
+    } while (yes_or_no("Would you like to play again"));
+
+    printf("Have a wonderful day!\n");
+}
+
+// Driver Code
+int main() {
+    int N = 100;
+
+    // Start the game
+    guess(N);
+
+    return 0;
+}
+
